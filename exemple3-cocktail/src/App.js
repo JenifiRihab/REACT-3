eba4fe12281fb61d6f35c8649c9fd561dcc5d86c
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-07-28 17:22:27 +0200

    my-commit

--- exemple3-cocktail/src/App.js
+++ exemple3-cocktail/src/App.js
@@ -7,6 +7,7 @@ import RandomCocktailPage from "./page/RandomCocktailPage";
 import "./App.css";
 import SearchResultsPage from "./page/SearchResultsPage";
 import ShowCocktailPage from "./page/ShowCocktailPage";
+import ListCocktailsByCategory from "./page/ListCocktailsByCategory";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/cocktails/show/:id" element={<ShowCocktailPage />} />
         <Route path="/cocktails/random" element={<RandomCocktailPage />} />
         <Route path="/search-results" element={<SearchResultsPage />} />
+        <Route path="/cocktails/by-category/:categoryName" element={<ListCocktailsByCategory/>} />
       </Routes>
     </BrowserRouter>
   );
--- exemple3-cocktail/src/page/ListCocktailsPage.jsx
+++ exemple3-cocktail/src/page/ListCocktailsPage.jsx
@@ -20,26 +20,29 @@ const ListCocktailsPage = () => {
   return (
     <>
       <Header />
+        <div>
+          <h2><strong> LIST OF COCKTAILS 🍹: </strong></h2>
+          <div className="list" >
+              {allCocktails.length === 0 && <p>Loading...</p>}
+              {allCocktails.map((cocktail) => (
+                <div className="category" key = {cocktail.idDrink}>
+                  <p>{cocktail.strDrink}</p>
+                  <Link 
+                    to={"/cocktails/by-category/" + cocktail.strCategory}>
+                      <br /> <button className="btn" >CATEGORIE : {cocktail.strCategory}</button>
+                  </Link>
 
-      <div>
-        <h2><strong> LIST OF COCKTAILS 🍹: </strong></h2>
-        
-        <div className="list" >
-
-            {allCocktails.length === 0 && <p>Loading...</p>}
-            {allCocktails.map((cocktail) => (
-
-              <div key = {cocktail.idDrink}>
-                <p>{cocktail.strDrink}</p>
-                <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
-                <Link to={"/cocktails/show/" + cocktail.idDrink}>
-                  <br />Voir le cocktail</Link>
-
-              </div>
-              
-            ))}
+                  <Link 
+                  to={"/cocktails/show/" + cocktail.idDrink}><br /> <button className="btn">VOIR LE COCKTAIL</button>
+                  </Link>
+                  
+                  <br />
+                  <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+                </div>
+                
+              ))}
+          </div>
         </div>
-      </div>
     </>
   );
 };
