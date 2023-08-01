d5de8b6661965d7ec8018b3848ab2d02a9db2d73
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-08-01 23:52:43 +0200

    my-commit

--- coworkings/src/page/CoworkingsPage.jsx
+++ coworkings/src/page/CoworkingsPage.jsx
@@ -1,49 +1,64 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import HeaderAdmin from "../component/HeaderAdmin";
+import Cookies from "js-cookie";
 
-  const CoworkingsPage = () => {
-    const [coworkings, setCoworkings] = useState([]);
-    const [deleteCoworkingMessage, setDeleteCoworkingMessage] = useState(null);
+const CoworkingsPage = () => {
+  const [coworkings, setCoworkings] = useState([]);
+  const [deleteCoworkingMessage, setDeleteCoworkingMessage] = useState(null);
 
-    const fetchCoworkings = async () => {
-      const response = await fetch("http://localhost:3010/api/coworkings", {
-        method: "GET",
-      });
-      const responseJs = await response.json();
+  const fetchCoworkings = async () => {
+    const response = await fetch("http://localhost:3010/api/coworkings", {
+      method: "GET",
+    });
+    const responseJs = await response.json();
 
-      setCoworkings(responseJs.data);
-    };
+    setCoworkings(responseJs.data);
+  };
 
-  
-    useEffect(() => {
-      fetchCoworkings();
-    }, [deleteCoworkingMessage]);
+  // useEffect avec la variable deleteCoworkingMessage
+  // dans le tableau, permet de dire qu'on executer
+  // la fonction fetchCoworkings à chaque fois que
+  // la variable deleteCoworkingMessage est modifiée
+  useEffect(() => {
+    fetchCoworkings();
+  }, [deleteCoworkingMessage]);
 
-    const handleDeleteCoworking = async (coworkingId) => {
-      const responseDelete = await fetch(`http://localhost:3010/api/coworkings/${coworkingId}`, {
-        method: "DELETE",
-      });
+  const handleDeleteCoworking = async (coworkingId) => {
+    const token = Cookies.get("jwt");
 
-      const responseDeleteJs = await responseDelete.json();
+    const responseDelete = await fetch(`http://localhost:3010/api/coworkings/${coworkingId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-      setDeleteCoworkingMessage(responseDeleteJs.message);
-    };
+    const responseDeleteJs = await responseDelete.json();
 
-    return (
-      <div className="coworking">
-        <h1> <strong> Liste des coworkings </strong></h1>
+    setDeleteCoworkingMessage(responseDeleteJs.message);
+  };
+
+  return (
+    <>
+      <HeaderAdmin />
+      <div>
+        <h1>Liste des coworkings</h1>
         {deleteCoworkingMessage && <p>{deleteCoworkingMessage}</p>}
         {coworkings.map((coworking) => (
           <div key={coworking.id}>
-              <h2>{coworking.name}</h2>
-              <p>
-                Adresse :{coworking.address.number} {coworking.address.street} - {coworking.address.postcode}
-                {coworking.address.city}
-              </p>
-              <button className="btn" onClick={() => handleDeleteCoworking(coworking.id)}>Supprimer le coworking</button>
+            <h2>{coworking.name}</h2>
+            <p>
+              Adresse :{coworking.address.number} {coworking.address.street} - {coworking.address.postcode}
+              {coworking.address.city}
+            </p>
+            <Link to={`/admin/coworkings/${coworking.id}/update`}>Mettre à jour le coworking</Link>
+            <button onClick={() => handleDeleteCoworking(coworking.id)}>Supprimer le coworking</button>
           </div>
         ))}
       </div>
-    );
-  };
+    </>
+  );
+};
 
 export default CoworkingsPage;
